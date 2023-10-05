da4ac593f3f8bcb61f2cbf40366784acdb30a0b6
adiazt01 <adiaztovar2004@gmail.com>
2023-10-05 16:40:39 -0400

    fix process env

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_PROJECTS = axios.create({
-  baseURL: import.meta.env.BACKEND_URL,
+  baseURL: `${import.meta.env.BACKEND_URL}/api`,
   withCredentials: true,
 });
 
