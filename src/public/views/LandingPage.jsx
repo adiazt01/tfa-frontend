a638b589186f1cd377ccff23aefc05d61f65000f
adiazt01 <adiaztovar2004@gmail.com>
2024-03-26 21:37:31 -0400

    Update backend URL and fix header styling on LandingPage

--- src/public/views/LandingPage.jsx
+++ src/public/views/LandingPage.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export const LandingPage = () => {
   console.log(import.meta.env.VITE_BACKEND_URL);
   return (
-    <div className="flex flex-col h-screen justify-center items-center bg-gray-900 text-white">
-      <header className="absolute top-0 left-0 w-full flex justify-between p-5">
+    <main className="flex flex-col h-screen justify-center items-center bg-gray-900 text-white">
+      <header className="fixed top-0 h-14 border-b bg-gray-900 border-black/10 shadow w-full flex items-center justify-between p-5">
         <h1 className="font-semibold text-xl">📃TaskFlow</h1>
-        <nav>
+        <nav className="flex flex-row gap-4 items-center">
           <Link
             to="/auth/login"
-            className="text-blue-500 hover:text-blue-400 mx-2"
+            className="text-blue-500 px-5 border-blue-500 py-1 rounded-lg border-2 hover:text-white hover:border-white transition"
           >
             Login
           </Link>
@@ -22,12 +22,12 @@ export const LandingPage = () => {
         </nav>
       </header>
 
-      <main className="flex flex-col items-center justify-center text-center">
+      <section className="flex flex-col items-center justify-center text-center">
         <h2 className="text-3xl font-bold mb-2">Welcome to TaskFlow</h2>
         <p className="text-lg">
           Your solution for efficient project management.
         </p>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 };
